d9e1376f103cda1103d9a4238b06273dbe8fca00
Diego Alejandro Gamboa <diego.a.gamboa.p@gmail.com>
2017-05-15 09:39:16 -0500

    Add files via upload

---
+++ CloseSMBv1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace CloseSMBv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                PowerShellInstance.AddScript("Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Services\\LanmanServer\\Parameters' SMB1 -Type DWORD -Value 0 -Force");
+
+                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+                Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+
+                // loop through each output object item
+                foreach (PSObject outputItem in PSOutput)
+                {
+                    // if null object was dumped to the pipeline during the script then a null
+                    // object may be present here. check for null to prevent potential NRE.
+                    if (outputItem != null)
+                    {
+                        //TODO: do something with the output item 
+                        Console.WriteLine(outputItem.BaseObject.GetType().FullName);
+                        Console.WriteLine(outputItem.BaseObject.ToString() + "\n");
+                    }
+                }
+
+
+                if (PowerShellInstance.Streams.Error.Count > 0)
+                {
+                    // error records were written to the error stream.
+                    // do something with the items found.
+                    Console.WriteLine("Error al intentar parchar");
+                }
+                else
+                {
+                    Console.WriteLine("Sistema parchado exitosamente");
+                }
+
+            }
+
+
+            Console.ReadLine();
+
+            
+
+
+
+        }
+    }
+}
